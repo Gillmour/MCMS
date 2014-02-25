5e484347cdc03fd3f3859c5abbc41a3a15f67721
mehdy11 <mhedychek@hotmail.fr>
2014-02-25 19:10:39 +0100

    Update verifpseudo_inscription.php

--- actionPHP/verifpseudo_inscription.php
+++ actionPHP/verifpseudo_inscription.php
@@ -3,7 +3,7 @@ require_once(getenv('DOCUMENT_ROOT').'/core.php');
 
 if(isset($_GET['pseudo']))
 {
-    $verif = $sql->prepare("SELECT  FROM comptes WHERE pseudo = :pseudo");
+    	$verif = $sql->prepare("SELECT  FROM comptes WHERE pseudo = :pseudo");
 	$verif->execute(Array(":pseudo" => $_GET['pseudo']));
 	
 	if(verifPseudoDejaUtiliser($_GET['pseudo']))
